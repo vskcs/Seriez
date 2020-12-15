cb5a2f9b63e6a357951a17e9d6fa257c9b55c6f6
VS Koushik <49363802+vskcs@users.noreply.github.com>
2020-12-16 00:24:59 +0530

    Update navBar.php

--- navBar.php
+++ navBar.php
@@ -42,7 +42,7 @@ if(isset($_SESSION['role'])){
 }
 </style>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+<nav class="navbar navbar-expand-lg navbar-light bg-warning ">
     <a class="navbar-brand" href="#">S E R I E Z</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
